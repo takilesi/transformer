c6eedb824f5328b84073319db3dc5ea5f47b133b
Doug <124453486+takilesi@users.noreply.github.com>
2023-06-19 22:41:30 -0400

    first version: '

---
+++ app.js
@@ -0,0 +1,142 @@
+// DOM MANIPULATION
+
+// Goal: Retail Site 
+    // Dynamically change the Entire Site with a click of a button
+    // General -- Mens -- Womens -- Children
+
+
+    let majorContainer ={
+        mens: {
+            headImage: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+            optionTags: ["Boots", "Flannels", "Rugged Wear", "Shaving Cream"],
+            productImages: [
+                {
+                    name: 'Boots',
+                    pic: "https://images.unsplash.com/photo-1527010154944-f2241763d806?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
+                },
+                {
+                    name: 'Boots',
+                    pic: "https://images.unsplash.com/photo-1638247025967-b4e38f787b76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+                },
+                {
+                    name: 'Flannels',
+                    pic: "https://images.unsplash.com/photo-1638718297700-e828368a54e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Scarves',
+                    pic: "https://images.unsplash.com/photo-1520903920243-00d872a2d1c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Hoodies',
+                    pic: "https://images.unsplash.com/photo-1509942774463-acf339cf87d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Hats',
+                    pic: "https://images.unsplash.com/photo-1521119989659-a83eee488004?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=723&q=80"
+                },
+            ],
+            textDescription: 'Update your everyday wardrobe with our collection of men’s pants. Discover tailored suit pants for work or special occasions, as well as comfier sweatpants and joggers for downtime. Mix up your weekend look and swap the jeans for a pair of cargo pants or chinos. Browse a range of colors, from neutral blacks and grays to brighter shades and prints. Find everything from soft breathable cotton to functional track pants and luxe velvet dress pants.',
+            lowImage: 'https://images.unsplash.com/photo-1516826957135-700dedea698c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+        },
+
+        womens: {
+            headImage: "https://shopwildginger.com/files/2023/03/IMG_4471-dl.jpg?w=1600&a=t",
+            optionTags: ["High Heels", "Flannels", "Fancy Dresses", "Scented Soap"],
+            productImages: [
+                {
+                    name: 'Sneakers',
+                    pic: "https://images.unsplash.com/photo-1527010154944-f2241763d806?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"
+                },
+                {
+                    name: 'Boots',
+                    pic: "https://images.unsplash.com/photo-1638247025967-b4e38f787b76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
+                },
+                {
+                    name: 'Flannels',
+                    pic: "https://images.unsplash.com/photo-1638718297700-e828368a54e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Scarves',
+                    pic: "https://images.unsplash.com/photo-1520903920243-00d872a2d1c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Hoodies',
+                    pic: "https://images.unsplash.com/photo-1509942774463-acf339cf87d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                },
+                {
+                    name: 'Scented Soap',
+                    pic: "https://cdn.shopify.com/s/files/1/1127/6932/products/35105SG_5acbbe57-e51b-4744-8de1-98aa6e875112_842x.jpg?v=1677284690"
+                },
+            ],
+            textDescription: 'Update your everyday wardrobe with our collection of men’s pants. Discover tailored suit pants for work or special occasions, as well as comfier sweatpants and joggers for downtime. Mix up your weekend look and swap the jeans for a pair of cargo pants or chinos. Browse a range of colors, from neutral blacks and grays to brighter shades and prints. Find everything from soft breathable cotton to functional track pants and luxe velvet dress pants.',
+            lowImage: 'https://images.unsplash.com/photo-1516826957135-700dedea698c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+        },
+
+        kids: {}
+    }
+
+
+let mainImage = document.querySelector('.mainImg')
+let option = document.querySelector('.options')
+let miniOp = document.querySelectorAll('.choice')
+
+let productBox = document.querySelector('.productContainer')
+let pic = document.querySelector('.polaroid')
+let textBox= document.querySelector('.textContainer')
+
+
+
+const changeAll = () => {
+    changeTopPic()
+    changeOptions()
+    product()
+}
+
+let getMensWomensKids = document.getElementsByClassName('tabs')
+    if (getMensWomensKids[0] == 'Mens Wear'){
+
+        const changeTopPic = () =>{
+            // createImage 
+            let newimage = document.createElement('img')
+                newimage.setAttribute('src', majorContainer.mens.headImage)
+                newimage.setAttribute('class','topPic')
+            // replaceWith current
+            mainImage.replaceWith(newimage)
+
+        }
+
+        const changeOptions = () =>{
+            majorContainer.mens.optionTags.forEach((tagname)=>{
+                // CREATE ELE
+                let txt = document.createElement('div')
+                txt.innerHTML = tagname
+                // REPLACE ELEMENT WITH NEW ELEMENT
+                option.append(txt)
+            })
+        }
+
+        const product= () => {
+            majorContainer.mens.productImages.forEach((obj)=>{
+                let shopBox  = document.createElement('img')
+                shopBox.setAttribute('src',obj.pic)
+                shopBox.setAttribute('class','product')
+                productBox.append(shopBox)
+            })
+        }
+    }
+ 
+  
+
+
+
+
+
+    // const changeOptions = () =>{
+    //     option.replaceChildren();
+    //     majorContainer.mens.optionTags.forEach((tagname)=>{
+    //         // CREATE ELEMENT
+    //         let txt = document.createElement('div')
+    //         txt.innerHTML = tagname
+    //         // REPLACE ELEMENT WITH NEW ELEMENT
+    //         option.append(txt)
+    //     })  }
